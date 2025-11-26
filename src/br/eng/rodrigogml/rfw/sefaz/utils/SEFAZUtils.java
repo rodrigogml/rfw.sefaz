27d8e63a5fc817f71e20dfe943ad7918fa57fa60
Rodrigo Leitão <rodrigogml@gmail.com>
2025-11-25 22:33:12 -0300

    .

--- src/br/eng/rodrigogml/rfw/sefaz/utils/SEFAZUtils.java
+++ src/br/eng/rodrigogml/rfw/sefaz/utils/SEFAZUtils.java
@@ -2,9 +2,6 @@ package br.eng.rodrigogml.rfw.sefaz.utils;
 
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,18 +12,15 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
 
-import br.eng.rodrigogml.rfw.kernel.RFW;
 import br.eng.rodrigogml.rfw.kernel.exceptions.RFWCriticalException;
 import br.eng.rodrigogml.rfw.kernel.exceptions.RFWException;
-import br.eng.rodrigogml.rfw.kernel.exceptions.RFWValidationException;
 import br.eng.rodrigogml.rfw.kernel.interfaces.RFWCertificate;
 import br.eng.rodrigogml.rfw.kernel.preprocess.PreProcess;
+import br.eng.rodrigogml.rfw.kernel.utils.RUSerializer;
 import br.eng.rodrigogml.rfw.kernel.utils.RUString;
-import br.eng.rodrigogml.rfw.kernel.utils.RUTypes;
 import br.eng.rodrigogml.rfw.kernel.utils.RUXML;
 import br.eng.rodrigogml.rfw.sefaz.utils.SEFAZEnums.SEFAZ_WebServices;
 import br.eng.rodrigogml.rfw.sefaz.utils.SEFAZEnums.SEFAZ_tpAmb;
-import br.eng.rodrigogml.rfw.sefaz.utils.SEFAZEnums.SEFAZ_uf;
 
 import xsdobjects.consCad200.TConsCad;
 import xsdobjects.consCad200.TConsCad.InfCons;
@@ -58,8 +52,11 @@ public class SEFAZUtils {
    * @param rootClass Classe do objeto que repreta a tag rais do XML.
    * @return Objeto da Classe
    * @throws RFWException
+   * @Deprecated Similar ao método {@link RUSerializer#desserializeFromXML(String, Class)} no entanto este método já está em uso para NF no BIS2 e resolvi mantê-lo aqui até entender a real necessidade dele.
+   *
    */
   @SuppressWarnings("unchecked")
+  @Deprecated
   public static <T> T readXMLToObject(String xml, Class<T> rootClass) throws RFWException {
     PreProcess.requiredNonNull(xml);
     PreProcess.requiredNonNull(rootClass);
@@ -93,7 +90,9 @@ public class SEFAZUtils {
    * @param root Objecto raiz para ser serializado em XML.
    * @return String xml representando o conteúdo do objeto sem formatação.
    * @throws RFWException Lançado em caso de falha ou dados inválidos.
+   * @Deprecated Similar ao método {@link RUSerializer#serializeToXML(Object, Class)} no entanto este método já está em uso para NF no BIS2 e resolvi mantê-lo aqui até entender a real necessidade dele.
    */
+  @Deprecated
   public static String writeXMLFromObject(Object root) throws RFWException {
     PreProcess.requiredNonNull(root);
     try {
@@ -198,158 +197,6 @@ public class SEFAZUtils {
       versao = matcher.group(1);
     }
     return versao;
-  }
-
-  /**
-   * Faz o parser de um valor decimal encontrado na NFe para BigDecimal.<Br>
-   * Faz o mesmo que 'new BigDecimal(value)', porém já trata o conteúdo entre nulo e vazio, e valida se o conteúdo é válido com mensagem personalizada.
-   *
-   * @param value Valor a ser transformado em BigDecimal
-   * @return BigDecimal com o valor encontrato, ou nulo caso a string esteja vazia/nula.
-   * @throws RFWException
-   * @TODO Refatorar esse método para um prefixo 'parse', mais comum e migrar para o RUTypes
-   */
-  // TODO Refatorar esse método para um prefixo 'parse', mais comum e migrar para o RUTypes
-  public static BigDecimal convertBigDecimalToJava(String value) throws RFWException {
-    value = PreProcess.processStringToNull(value);
-    if (value != null) {
-      if (!value.matches("[0-9]+((\\.)?[0-9]+)?")) {
-        throw new RFWValidationException("RFW_000051", new String[] { value });
-      }
-      return new BigDecimal(value);
-    }
-    return null;
-  }
-
-  /**
-   * Faz o parser de um valor decimal encontrado na NFe para BigDecimal.<Br>
-   * Faz o mesmo que 'new BigDecimal(value)', porém já trata o conteúdo entre nulo e vazio, e valida se o conteúdo é válido com mensagem personalizada. <br>
-   * Permite definir o BigDecimal com um número exato de casas decimais.
-   *
-   * @param value Valor a ser transformado em BigDecimal
-   * @param decimals Quantidade de casas decimais.
-   * @return BigDecimal com o valor encontrato, ou nulo caso a string esteja vazia/nula.
-   * @throws RFWException
-   * @TODO Refatorar esse método para um prefixo 'parse', mais comum
-   */
-  // TODO Refatorar esse método para um prefixo 'parse', mais comum e migrar para o RUTypes
-  public static BigDecimal convertBigDecimalToJava(String value, Integer decimals) throws RFWException {
-    BigDecimal v = convertBigDecimalToJava(value);
-    if (v != null) v = v.setScale(decimals, RFW.getRoundingMode());
-    return v;
-  }
-
-  /**
-   * Este método junta todas as informações necessárias para montar a linha de informação ser usada para gerar o QRCode da NFCe.
-   *
-   * @param uf Estado do webservice usado para emitir a NFCe
-   * @param env Ambiente usado para emitir a NFCe
-   * @param chave chave de acesso da NFCe. (44 Bytes)
-   * @param destCPFCNPJ Documento CPF/CNPJ de identificação do cliente. Passar nulo caso o consumidor não seja identificado.
-   * @param dhEmi Data e Hora de emissão da NFCe.
-   * @param vNF Valor total da NFCe. BigDecimal já com escala correta de 2 dígitos.
-   * @param vICMS Valor total do ICMS da NFCe. BigDecimal já com escala correta de 2 dígitos.
-   * @param digestValue DigestValue da NFCe, retornado da SEFAZ após autorização. É espero um byte array em base 64. Esse padrão foi escolhido pois é assim que ele é retornado da SEFAZ.
-   * @param tokenID ID do token (código de segurança) sendo utilizado. Normalmente o identificador é "000001" ou "000002" dependendo do token gerado pelo estado.
-   * @param token Token (código de segurança) de conhecimento apenas da empresa e da Receita Federal Estadual. Obtido pelo site ou diretamente da aplicação pelo webservice.
-   *
-   * @return
-   */
-  public static String generateQRCodeData(SEFAZ_uf uf, SEFAZ_tpAmb env, String chave, String destCPFCNPJ, LocalDateTime dhEmi, BigDecimal vNF, BigDecimal vICMS, byte[] digestValue, String tokenID, String token) throws RFWException {
-    return generateQRCodeData(uf, env, chave, destCPFCNPJ, RUTypes.formatToyyyy_MM_dd_T_HH_mm_ssXXX(dhEmi), vNF, vICMS, digestValue, tokenID, token);
-  }
-
-  /**
-   * Este método junta todas as informações necessárias para montar a linha de informação ser usada para gerar o QRCode da NFCe.
-   *
-   * @param uf Estado do webservice usado para emitir a NFCe
-   * @param env Ambiente usado para emitir a NFCe
-   * @param chave chave de acesso da NFCe. (44 Bytes)
-   * @param destCPFCNPJ Documento CPF/CNPJ de identificação do cliente. Passar nulo caso o consumidor não seja identificado.
-   * @param dhEmi Data e Hora de emissão da NFCe.
-   * @param vNF Valor total da NFCe. BigDecimal já com escala correta de 2 dígitos.
-   * @param vICMS Valor total do ICMS da NFCe. BigDecimal já com escala correta de 2 dígitos.
-   * @param digestValue DigestValue da NFCe, retornado da SEFAZ após autorização. É espero um byte array em base 64. Esse padrão foi escolhido pois é assim que ele é retornado da SEFAZ.
-   * @param tokenID ID do token (código de segurança) sendo utilizado. Normalmente o identificador é "000001" ou "000002" dependendo do token gerado pelo estado.
-   * @param token Token (código de segurança) de conhecimento apenas da empresa e da Receita Federal Estadual. Obtido pelo site ou diretamente da aplicação pelo webservice.
-   *
-   * @return
-   */
-  public static String generateQRCodeData(SEFAZ_uf uf, SEFAZ_tpAmb env, String chave, String destCPFCNPJ, Date dhEmi, BigDecimal vNF, BigDecimal vICMS, byte[] digestValue, String tokenID, String token) throws RFWException {
-    return generateQRCodeData(uf, env, chave, destCPFCNPJ, RUTypes.formatToyyyy_MM_dd_T_HH_mm_ssXXX(dhEmi), vNF, vICMS, digestValue, tokenID, token);
-  }
-
-  /**
-   * Este método junta todas as informações necessárias para montar a linha de informação ser usada para gerar o QRCode da NFCe.
-   *
-   * @param uf Estado do webservice usado para emitir a NFCe
-   * @param env Ambiente usado para emitir a NFCe
-   * @param chave chave de acesso da NFCe. (44 Bytes)
-   * @param destCPFCNPJ Documento CPF/CNPJ de identificação do cliente. Passar nulo caso o consumidor não seja identificado.
-   * @param dhEmi Data e Hora de emissão da NFCe no formato UTC utilizado no XML.
-   * @param vNF Valor total da NFCe. BigDecimal já com escala correta de 2 dígitos.
-   * @param vICMS Valor total do ICMS da NFCe. BigDecimal já com escala correta de 2 dígitos.
-   * @param digestValue DigestValue da NFCe, retornado da SEFAZ após autorização. É espero um byte array em base 64. Esse padrão foi escolhido pois é assim que ele é retornado da SEFAZ.
-   * @param tokenID ID do token (código de segurança) sendo utilizado. Normalmente o identificador é "000001" ou "000002" dependendo do token gerado pelo estado.
-   * @param token Token (código de segurança) de conhecimento apenas da empresa e da Receita Federal Estadual. Obtido pelo site ou diretamente da aplicação pelo webservice.
-   *
-   * @return
-   */
-  public static String generateQRCodeData(SEFAZ_uf uf, SEFAZ_tpAmb env, String chave, String destCPFCNPJ, String dhEmi, BigDecimal vNF, BigDecimal vICMS, byte[] digestValue, String tokenID, String token) throws RFWException {
-    // Parte 1: Endereço de consulta
-    String address = null;
-    if (uf == SEFAZ_uf.SP) {
-      if (env == SEFAZ_tpAmb.HOMOLOGACAO) {
-        address = "https://www.homologacao.nfce.fazenda.sp.gov.br/NFCeConsultaPublica/Paginas/ConsultaQRCode.aspx?";
-      } else if (env == SEFAZ_tpAmb.PRODUCAO) {
-        address = "https://www.nfce.fazenda.sp.gov.br/NFCeConsultaPublica/Paginas/ConsultaQRCode.aspx?";
-      }
-    } else if (uf == SEFAZ_uf.RS) {
-      if (env == SEFAZ_tpAmb.HOMOLOGACAO) {
-        address = "https://www.sefaz.rs.gov.br/NFCE/NFCE-COM.aspx?";
-      } else if (env == SEFAZ_tpAmb.PRODUCAO) {
-        address = "https://www.sefaz.rs.gov.br/NFCE/NFCE-COM.aspx?";
-      }
-    }
-
-    // Valida se temos um endereço
-    if (address == null) {
-      throw new RFWCriticalException("BISModules_000095", new String[] { "" + uf, "" + env });
-    }
-
-    // Parte 2: Parametros
-    final StringBuilder buff = new StringBuilder();
-    buff.append("chNFe=").append(chave);
-    buff.append("&nVersao=100");
-    buff.append("&tpAmb=").append(env.getXmlData());
-    if (destCPFCNPJ != null) buff.append("&cDest=").append(destCPFCNPJ);
-    buff.append("&dhEmi=").append(RUString.toHex(dhEmi));
-    buff.append("&vNF=").append(vNF.toString());
-    buff.append("&vICMS=").append(vICMS.toString());
-    buff.append("&digVal=").append(RUString.toHexFromBase64(digestValue));
-    buff.append("&cIdToken=").append(tokenID);
-
-    // Parte 3 - Calculamos a Hash com base nos parametros concatenados na parte 2 e a chave de segurança. Do resultado é criado o parmetro hashQRCode e concatenado no final.
-    String tosha = buff.toString() + token; // String da qual precisamos calcular a hash sha1
-    String hash = RUString.calcSHA1ToHex(tosha);
-    buff.append("&cHashQRCode=").append(hash);
-
-    // Por fim retornamos o endereço base, os parametros da parte 2 (e 3 que foi includo no buffer da parte 2)
-    return address + buff.toString();
-  }
-
-  /**
-   * Este método interpreta todos os formatos já encontrados de datas que podem vir na NFe para o formato do Java. Atualmente os formatos reconhecidos são:<br>
-   * <li>"yyyy-MM-dd'T'HH:mm:ssXXX" (Padrão UTC)</li>
-   * <li>"yyyy-MM-dd'T'HH:mm:ss" (Padrão UTC Sem TimeZone)</li>
-   * <li>"yyyy-MM-dd"</li>
-   *
-   * @param date
-   * @return
-   * @throws RFWException
-   */
-  public static Date parseDate(String date) throws RFWException {
-    return RUTypes.parseDate(date);
   }
 
   /**
